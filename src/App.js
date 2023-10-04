60816dcb1d17cb1a96f3e9ce17df21c8839c4bd8
Sammienator <sammugo18@gmail.com>
2023-10-05 02:33:54 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from "../src/components/Home";
-import Gallery from './components/Gallery';
+
 import Best from './components/Best';
 import Exclusive from './components/Exclusive';
 import Customized from './components/Customized';
 import Testimonial from './components/Testimonial';
 import Footer from './components/Footer';
+import PricingSection from './components/PricingSection';
 
 function App() {
   return (
     <>
     <Home/>
-    <Gallery/>
+    <PricingSection/>
+   
     <Best/>
     <Exclusive/>
     <Customized/>
